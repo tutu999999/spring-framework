24097052b8561a2929f2234da9a88ba093315233
tuyl <tuyuelai@szhtxx.com>
2022-03-01 16:34:39 +0800

    feat： 暂时解决日志不输出到console问题

--- myspring/src/main/java/test1/Test.java
+++ myspring/src/main/java/test1/Test.java
@@ -1,10 +1,16 @@
 package test1;
 
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+
+
 public class Test {
-    public static void main(String[] args) {
-		System.out.println("user dao");
+	private static Logger logger = LoggerFactory.getLogger(Test.class);
+	public static void main(String[] args) {
+		logger.info("user dao");
 		AnnotationConfigApplicationContext ac=new
                 AnnotationConfigApplicationContext(AnnotationConfig.class);
         UserDao userDao = ac.getBean(UserDao.class);
