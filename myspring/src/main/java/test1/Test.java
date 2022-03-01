634c7631233c3ce7142c3f9cbc6eb06509b258a0
tuyl <tuyuelai@szhtxx.com>
2022-03-01 16:20:40 +0800

    feat： version1

---
+++ myspring/src/main/java/test1/Test.java
@@ -0,0 +1,13 @@
+package test1;
+
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+public class Test {
+    public static void main(String[] args) {
+		System.out.println("user dao");
+		AnnotationConfigApplicationContext ac=new
+                AnnotationConfigApplicationContext(AnnotationConfig.class);
+        UserDao userDao = ac.getBean(UserDao.class);
+        userDao.printInfo();
+    }
+}
