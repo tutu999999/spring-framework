d391b9e610f870463e31459cfcf3ef2b9aa63750
tuyl <tuyuelai@szhtxx.com>
2022-03-01 17:18:24 +0800

    feat： 暂时解决日志不输出到console问题

--- myspring/src/main/java/test1/UserDao.java
+++ myspring/src/main/java/test1/UserDao.java
@@ -1,8 +1,50 @@
 package test1;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.*;
+import org.springframework.beans.factory.config.BeanPostProcessor;
 import org.springframework.stereotype.Repository;
 @Repository
-public class UserDao {
-    public void printInfo(){
+public class UserDao implements BeanFactoryAware, BeanNameAware, InitializingBean, DisposableBean, BeanPostProcessor {
+	private static Logger logger = LoggerFactory.getLogger(UserDao.class);
+
+	private String a;
+
+	public UserDao() {
+		logger.info("userDao{}", "构造方法");
+	}
+
+	public void printInfo(){
         System.out.println("user dao");
     }
+
+	@Override
+	public void setBeanFactory(BeanFactory beanFactory) throws BeansException {
+		logger.info("userDao{}", "setBeanFactory");
+	}
+
+	@Override
+	public void setBeanName(String name) {
+		logger.info("userDao{}", "setBeanName");
+	}
+
+	@Override
+	public void destroy() throws Exception {
+
+	}
+
+	@Override
+	public void afterPropertiesSet() throws Exception {
+
+	}
+
+	public String getA() {
+		return a;
+	}
+
+	public void setA(String a) {
+		logger.info("userDao{}", "execute set");
+		this.a = a;
+	}
 }
